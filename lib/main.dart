001e6d51298a18ffecbaeb8b539a3f0796491f9a
MohammedEhap007 <mohammedehap007@gmail.com>
2025-11-02 17:26:57 +0200

    feat: Add dependency injection setup with GetIt for AuthService and LoginCubit

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+import 'core/di/dependency_injection.dart';
 import 'core/helpers/cache_helper.dart';
 import 'core/utils/app_bloc_observer.dart';
 import 'laza_eshop.dart';
@@ -11,5 +12,6 @@ Future<void> main() async {
   Bloc.observer = AppBlocObserver();
   await ScreenUtil.ensureScreenSize();
   await CacheHelper.init();
+  await setupGetIt();
   runApp(const LazaEshop());
 }
