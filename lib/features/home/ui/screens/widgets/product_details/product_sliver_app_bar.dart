404de32d56374dd8693a9985f58b1849756a678d
MohammedEhap007 <mohammedehap007@gmail.com>
2025-11-27 03:16:02 +0200

    feat: Refactor ProductSliverAppBar to use separate leading and action widgets for improved readability and maintainability.
    - Add getCategoryLogo function to product sliver app bar

--- lib/features/home/ui/screens/widgets/product_details/product_sliver_app_bar.dart
+++ lib/features/home/ui/screens/widgets/product_details/product_sliver_app_bar.dart
@@ -6,13 +6,20 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:laza_eshop/core/themes/app_colors.dart';
 import 'package:laza_eshop/core/utils/app_extensions.dart';
+import 'package:laza_eshop/features/home/data/models/categories_item_model.dart';
 import 'package:laza_eshop/features/home/data/models/products_item_model.dart';
 import 'package:laza_eshop/features/home/ui/screens/widgets/products/product_item.dart';
 
 import '../../../../../../core/themes/app_assets.dart';
+import '../../../../../../core/themes/app_text_styles.dart';
+import 'product_sliver_app_bar_actions.dart';
+import 'product_sliver_app_bar_leading.dart';
 
 class ProductSliverAppBar extends StatelessWidget {
-  const ProductSliverAppBar({super.key, required this.product});
+  const ProductSliverAppBar({
+    super.key,
+    required this.product,
+  });
 
   final ProductsItemModel product;
 
@@ -24,55 +31,9 @@ class ProductSliverAppBar extends StatelessWidget {
       pinned: true,
       stretch: true,
       leadingWidth: 60.w,
-      leading: Padding(
-        padding: const EdgeInsets.only(left: 20.0, top: 5.0),
-        child: CircleAvatar(
-          radius: 22.5.r,
-          backgroundColor: Theme.of(
-            context,
-          ).colorScheme.surfaceContainerHighest,
-          child: IconButton(
-            padding: EdgeInsets.zero,
-            onPressed: () {
-              context.pop();
-            },
-            icon: SvgPicture.asset(
-              AppAssets.iconsArrowBackLightIcon,
-              width: 25.sp,
-              height: 25.sp,
-              colorFilter: ColorFilter.mode(
-                Theme.of(context).iconTheme.color!,
-                BlendMode.srcIn,
-              ),
-              fit: BoxFit.scaleDown,
-            ),
-          ),
-        ),
-      ),
-      actions: [
-        Padding(
-          padding: const EdgeInsets.only(right: 20.0, top: 5.0),
-          child: CircleAvatar(
-            radius: 22.5.r,
-            backgroundColor: Theme.of(
-              context,
-            ).colorScheme.surfaceContainerHighest,
-            child: IconButton(
-              padding: EdgeInsets.zero,
-              onPressed: () {},
-              icon: SvgPicture.asset(
-                AppAssets.iconsCartIcon,
-                width: 25.sp,
-                height: 25.sp,
-                colorFilter: ColorFilter.mode(
-                  Theme.of(context).iconTheme.color!,
-                  BlendMode.srcIn,
-                ),
-                fit: BoxFit.scaleDown,
-              ),
-            ),
-          ),
-        ),
+      leading: const ProductSliverAppBarLeading(),
+      actions: const [
+        ProductSliverAppBarActions(),
       ],
       flexibleSpace: FlexibleSpaceBar(
         titlePadding: EdgeInsets.zero,
@@ -94,10 +55,24 @@ class ProductSliverAppBar extends StatelessWidget {
               ),
               child: Padding(
                 padding: const EdgeInsets.all(4.0),
-                child: SvgPicture.network(
-                  'https://upload.wikimedia.org/wikipedia/commons/a/a6/Logo_NIKE.svg',
-                  fit: BoxFit.scaleDown,
-                ),
+                child: getCategoryLogoUrl() != null
+                    ? SvgPicture.network(
+                        getCategoryLogoUrl()!,
+                        placeholderBuilder: (context) => Image.asset(
+                          AppAssets.iconsCategoriesLoadingIcon,
+                          fit: BoxFit.contain,
+                        ),
+                        fit: BoxFit.scaleDown,
+                      )
+                    : Center(
+                        child: Text(
+                          product.name ?? '',
+                          style: AppTextStyles.font16SemiBold,
+                          textAlign: TextAlign.center,
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
               ),
             ),
           ),
@@ -111,5 +86,40 @@ class ProductSliverAppBar extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  String? getCategoryLogoUrl() {
+    if (product.categories == null || product.categories!.isEmpty) {
+      return null;
+    }
+
+    switch (product.categories!.first) {
+      case 'gucci':
+        return 'https://upload.wikimedia.org/wikipedia/commons/c/c5/Gucci_logo.svg';
+      case 'balenciaga':
+        return 'https://upload.wikimedia.org/wikipedia/commons/4/4b/Balenciaga_Logo.svg';
+      case 'adidas':
+        return 'https://upload.wikimedia.org/wikipedia/commons/2/20/Adidas_Logo.svg';
+      case "levi's":
+        return 'https://upload.wikimedia.org/wikipedia/commons/7/75/Levi%27s_logo.svg';
+      case 'nike':
+        return 'https://upload.wikimedia.org/wikipedia/commons/a/a6/Logo_NIKE.svg';
+      case 'puma':
+        return 'https://upload.wikimedia.org/wikipedia/en/d/da/Puma_complete_logo.svg';
+      case 'zara':
+        return 'https://upload.wikimedia.org/wikipedia/commons/f/fd/Zara_Logo.svg';
+      case 'under armour':
+        return 'https://upload.wikimedia.org/wikipedia/commons/4/44/Under_armour_logo.svg';
+      case 'tommy hilfiger':
+        return 'https://upload.wikimedia.org/wikipedia/commons/2/26/Tommy_hilfig_vectorlogo.svg';
+      case 'h&m':
+        return 'https://upload.wikimedia.org/wikipedia/commons/5/53/H%26M-Logo.svg';
+      case 'reebok':
+        return 'https://upload.wikimedia.org/wikipedia/commons/5/53/Reebok_2019_logo.svg';
+      case 'lacoste':
+        return 'https://upload.wikimedia.org/wikinews/en/4/43/Lacoste_logo.svg';
+      default:
+        return null;
+    }
   }
 }
